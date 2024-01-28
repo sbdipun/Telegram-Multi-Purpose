c023a54403f0edb0acd95eeb79e452956d2e999b
sbdipun <87560288+sbdipun@users.noreply.github.com>
2024-01-28 23:01:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -111,13 +111,7 @@ async def media_filter(bot, update):
             disable_web_page_preview=True
         )
         return
-    
-    # pixeldrain data
-    text = f"**File Name:** `{data['name']}`" + "\n"
-    text += f"**Download Page:** `https://pixeldrain.com/u/{data['id']}`" + "\n"
-    text += f"**Direct Download Link:** `https://pixeldrain.com/api/file/{data['id']}`" + "\n"
-    text += f"**Upload Date:** `{data['date_upload']}`" + "\n"
-    text += f"
+
     
     # pixeldrain data
     text = f"**File Name:** `{data['name']}`" + "\n"
